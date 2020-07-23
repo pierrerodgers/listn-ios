cb02a107ea2a4dd3c2d1ecfaea1c8be6c8f352a2
Pierre Rodgers <pierre.rodgers.1@gmail.com>
2020-07-23 12:51:13 +1000

    Create LoginViewModel.swift

---
+++ listn/LoginViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  LoginViewModel.swift
+//  listn
+//
+//  Created by Pierre Rodgers on 23/7/20.
+//  Copyright © 2020 Pierre Rodgers. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewModel: ObservableObject {
+    
+}
