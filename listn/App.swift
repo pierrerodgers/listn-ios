f552a234b03c70e4729265466f086ebc93eed703
Pierre Rodgers <pierre.rodgers.1@gmail.com>
2020-07-23 12:51:10 +1000

    Update App.swift
    add code for signing in

--- listn/App.swift
+++ listn/App.swift
@@ -10,24 +10,40 @@ import Foundation
 import RealmSwift
 
 protocol LoginService {
-    func signUp(email:String, password:String)
+    func signUp(username:String, password:String)
     
-    func logIn(email:String, password:String)
+    func logIn(username:String, password:String)
+    
+    isLoggedIn : Bool
+    
+    error : String?
     
 }
 
 class MongoLoginService : LoginService {
+    // We assume that app is intitialised
     var app : RealmApp
+    
     
     init(app:RealmApp) {
         self.app = app
     }
     
-    func signUp(email: String, password: String) {
-        
+    func signUp(username: String, password: String) {
+        app.usernamePasswordProviderClient().registerEmail(username, password:password, completion: { error in
+            guard error == nil else {
+                print("Signup failed")
+                self.error = error
+                return
+            }
+            print("Signup successful")
+            self.logIn(username: username, password: password)
+        })
     }
     
-    func logIn(email: String, password: String) {
-        
+    func logIn(username: String, password: String) {
+        app.login(withcredential:AppCredentials(username: username, password:password)) { [weak self] in
+            self!.isLoggedIn = true
+        }
     }
 }
