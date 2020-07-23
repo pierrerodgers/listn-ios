6fa1eba8e1fe63e8d502ec74cfa683cd3abe657e
Pierre Rodgers <pierre.rodgers.1@gmail.com>
2020-07-23 12:13:15 +1000

    add loginview

---
+++ listn/LoginView.swift
@@ -0,0 +1,21 @@
+//
+//  LoginView.swift
+//  listn
+//
+//  Created by Pierre Rodgers on 23/7/20.
+//  Copyright © 2020 Pierre Rodgers. All rights reserved.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
