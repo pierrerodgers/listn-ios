514ea6e294f0ec13dc6fafd886b283f418e56984
Pierre Rodgers <pierre.rodgers.1@gmail.com>
2020-07-24 14:59:55 +1000

    Create FeedModel.swift

---
+++ listn/Models/FeedModel.swift
@@ -0,0 +1,35 @@
+//
+//  FeedModel.swift
+//  listn
+//
+//  Created by Pierre Rodgers on 24/7/20.
+//  Copyright © 2020 Pierre Rodgers. All rights reserved.
+//
+
+import UIKit
+import Combine
+
+class FeedModel: ObservableObject {
+    // MARK: Overview
+    /*
+        This class should be independent of whatever database mechanism
+        I end up using.
+     
+        It should only handle UI logic
+     */
+    
+    var app : AppData
+    
+    @Published var reviews : Array<Review>
+    
+    
+    
+    init(app: AppData) {
+        self.app = app
+        reviews = app.getLatestReviews()
+    }
+    
+    
+    
+    
+}
