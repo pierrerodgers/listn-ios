465bbdde71568a0d98c712c711765af2f14f108c
Pierre Rodgers <pierre.rodgers.1@gmail.com>
2020-07-28 15:26:50 +1000

    Update App.swift
    Start ListnApp class

--- listn/App.swift
+++ listn/App.swift
@@ -9,6 +9,92 @@
 import Foundation
 import RealmSwift
 
+class ListnApp {
+    
+    var realmApp : RealmApp!
+    var loginService : LoginService!
+    var user : User?
+    var realm : Realm?
+    var appData : AppData?
+    
+    var isLoggedIn : Bool {
+        get {
+            return loginService.isLoggedIn
+        }
+    }
+    
+    init() {
+        // Initialise Realm App
+        let config = AppConfiguration(baseURL: "https://realm.mongodb.com", transport: nil, localAppName: nil, localAppVersion: nil)
+        realmApp = RealmApp(id:"listn-bsliv", configuration: config)
+        
+        // Initialise login service and fill variables if logged in
+        loginService = MongoLoginService(app: realmApp)
+        if loginService.isLoggedIn {
+            realm = try! Realm(configuration: app.currentUser()!.configuration(partitionValue:(app.currentUser()?.identity)!))
+            Network.shared.token = app.currentUser()!.accessToken!
+            user = realm?.objects(User.self)[0]
+        }
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+}
+
+class ListnAppData : AppData {
+    // Assumes that token is already added to Network class!
+    
+    func getLatestReviews() -> Array<Review> {
+        return []
+    }
+    
+    func getAlbums(artist: Artist) -> Array<Album> {
+        /*Network.shared.token = (app.currentUser()?.accessToken!)!
+        print(Network.shared.token)
+        Network.shared.apollo.fetch(query: AlbumQuery(query:AlbumQueryInput(name:"Punisher"))) { result in
+            switch result {
+            case .success(let graphQlResult):
+                print("success! Result:\(graphQlResult.data?.album?.artist?.name)")
+            case .failure(let error) :
+                print(error)
+            }
+            
+            
+        }*/
+        /*
+        Network.shared.apollo.fetch(query: AlbumQuery(query: AlbumQueryInput(artist:ArtistQueryInput(_id: artist._id!.stringValue)))) { result in
+            switch result {
+            case .success(let graphQlResult):
+                return graphQlResult.data?.albums
+            case .failure(let error) :
+                print(error)
+                return []
+            }
+        }*/
+        
+        return []
+    }
+    
+    func getReviews(artist: Artist) -> Array<Review> {
+        return []
+    }
+    
+    func getReviews(album: Album) -> Array<Review> {
+        return []
+    }
+    
+    func getReviews(user: User) -> Array<Review> {
+        return []
+    }
+    
+    
+}
+
 protocol LoginService {
     func signUp(username:String, password:String, completion:@escaping (Error?) -> Void)
     
@@ -48,8 +134,11 @@ class MongoLoginService : LoginService {
     var isLoggedIn: Bool
     
     
+    
     init(app:RealmApp) {
         self.app = app
+        
+        // Check if User is looged in (THIS CODE NEEDS TO BE FIXED!!
         if (app.currentUser() != nil) {
             isLoggedIn = true
         }
