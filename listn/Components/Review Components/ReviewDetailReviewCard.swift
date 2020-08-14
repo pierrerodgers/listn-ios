0686b6d5cfff7770b9caf495de2ac8f37fbba270
Pierre Rodgers <pierre.rodgers.1@gmail.com>
2020-08-14 12:36:27 +1000

    Create ReviewDetailReviewCard.swift

---
+++ listn/Components/Review Components/ReviewDetailReviewCard.swift
@@ -0,0 +1,46 @@
+//
+//  ReviewDetailReviewCard.swift
+//  listn
+//
+//  Created by Pierre Rodgers on 14/8/20.
+//  Copyright © 2020 Pierre Rodgers. All rights reserved.
+//
+
+import SwiftUI
+import SDWebImageSwiftUI
+
+struct ReviewDetailReviewCard: View {
+    let review : ListnReview
+    //let comments : Array<ListnComment>
+    
+    var body: some View {
+        VStack (alignment:.leading){
+            HStack{
+                // Reviewer/User image
+                Circle().frame(width:40, height:40)
+                Text("@\(review.username)").title()
+                Spacer()
+                Text(review.score).scoreText()
+            }
+            Text("Comments to come")
+            if(review.reviewType == .critic) {
+                FullReviewButton(link: review.link ?? "")
+            }
+        }
+    }
+}
+
+struct ReviewComments {
+    
+}
+
+
+
+struct ReviewDetailReviewCard_Previews: PreviewProvider {
+    
+    static var previews: some View {
+        let review = ListnCriticReview(forPreview: true)
+
+        return ReviewDetailReviewCard(review: review)
+    }
+}
