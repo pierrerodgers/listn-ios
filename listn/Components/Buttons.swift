3a1c37a34c635247f7ffd4542259510a65c03b97
Pierre Rodgers <pierre.rodgers.1@gmail.com>
2020-08-15 17:10:16 +1000

    Update Buttons.swift
    Add button stack

--- listn/Components/Buttons.swift
+++ listn/Components/Buttons.swift
@@ -26,11 +26,62 @@ struct FullReviewButton : View {
     }
 }
 
+struct SpotifyButton : View {
+    var link : String
+    
+    var body: some View {
+        Button(action: {}) {
+            HStack{
+                Image(systemName: "arrow.up.right.square").font(.system(size: 30))
+                Text("Listen on Spotify").font(.system(size: 20)).bold()
+            }.frame(minWidth:0, maxWidth: .infinity, minHeight:50, maxHeight: .infinity).padding(5).foregroundColor(.white).background(Color(red: 30/255, green: 215/255, blue: 96/255)).cornerRadius(10)
+        }.buttonStyle(PlainButtonStyle())
+    }
+}
+
+struct AppleMusicButton : View {
+    var link : String
+    
+    var body: some View {
+        Button(action: {}) {
+            HStack{
+                Image(systemName: "arrow.up.right.square").font(.system(size: 30))
+                Text("Listen on Apple Music").font(.system(size: 20)).bold()
+            }.frame(minWidth:0, maxWidth: .infinity, minHeight:50, maxHeight: .infinity).padding(5).foregroundColor(.white).background(Color(red: 235/255, green: 87/255, blue: 87/255)).cornerRadius(10)
+        }.buttonStyle(PlainButtonStyle())
+    }
+}
+
+struct ShareButton : View {
+    var body: some View {
+        Button(action: {}) {
+            HStack{
+                Image(systemName: "arrow.up.right.square").font(.system(size: 30))
+                Text("Share with friends").font(.system(size: 20)).bold()
+            }.frame(minWidth:0, maxWidth: .infinity, minHeight:50, maxHeight: .infinity).padding(5).foregroundColor(.white).background(Color.black).cornerRadius(10)
+        }.buttonStyle(PlainButtonStyle())
+    }
+}
+
+struct ButtonStack : View {
+    var streamingUrls: ListnStreamingUrls
+    
+    var body : some View {
+        VStack {
+            AppleMusicButton(link: streamingUrls.appleMusic)
+            SpotifyButton(link: streamingUrls.spotify)
+            ShareButton()
+        }
+    }
+}
+
 struct Buttons_Previews: PreviewProvider {
     static var previews: some View {
         Group {
             FullReviewButton(link:"").previewDisplayName("Full review button")
             RaveButton().previewDisplayName("Rave button")
+            SpotifyButton(link: "").previewDisplayName("Spotify button")
+            AppleMusicButton(link: "").previewDisplayName("Apple Music button")
         }.previewLayout(PreviewLayout.fixed(width: 400, height: 50))
         
     }
