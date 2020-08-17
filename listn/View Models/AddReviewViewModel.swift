1c8a746da4c02d7f1bc597ccffa98df434eed226
Pierre Rodgers <pierre.rodgers.1@gmail.com>
2020-08-17 12:23:35 +1000

    Update AddReviewViewModel.swift
    start error handling for add review view

--- listn/View Models/AddReviewViewModel.swift
+++ listn/View Models/AddReviewViewModel.swift
@@ -8,24 +8,35 @@
 
 import SwiftUI
 
+/*enum ReviewInputValid {
+    case missingScore, invalidScore, missingAlbum, valid
+}
+*/
+struct ReviewInputValid {
+    var missingScore = true
+    var invalidScore = false
+    var missingAlbum = true
+    var valid = false
+}
+
 class AddReviewViewModel: ObservableObject {
     
     var app : ListnApp
     
     @Published var album : ListnAlbum
+    @Published var inputValid : ReviewInputValid
     
     
     init(album:ListnAlbum, app: ListnApp) {
         self.album = album
         self.app = app
+        self.inputValid = ReviewInputValid()
     }
     
-    func postReview() {
-        let review = ListnUserReview(album:album, score:"100", text: "\(album.name)")
+    func postReview(review: NewReview) {
+        let review = ListnUserReview(album:album, score:String(review.score), text: review.text)
         
         app.postReview(review: review)
-        
-        
     }
     
 }
