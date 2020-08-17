37c2a34fdaed7eaeefdcba8f884976153b945f84
Pierre Rodgers <pierre.rodgers.1@gmail.com>
2020-08-17 15:00:56 +1000

    Update FeedView.swift
    Add floating NewRaveButton

--- listn/Views/FeedView.swift
+++ listn/Views/FeedView.swift
@@ -14,38 +14,48 @@ struct FeedView: View {
     @ObservedObject var model : FeedModel
     
     var body: some View {
-        NavigationView {
-            List {
-                ForEach(model.reviews.enumerated().map({$0}), id: \.element._id) { index, review in
-                    NavigationLink(destination: LazyView(ReviewDetailView(model: ReviewDetailViewModel(review: review, app: self.model.app))) ) {
-                        FeedReviewCard(review: review).listRowInsets(.init(top: 0, leading: 0, bottom: 0, trailing: 0)).onAppear() {
-                            if index == self.model.reviews.count - 25 {
-                                self.model.getNextPage()
+        ZStack {
+            
+            NavigationView {
+                List {
+                    ForEach(model.reviews.enumerated().map({$0}), id: \.element._id) { index, review in
+                        NavigationLink(destination: LazyView(ReviewDetailView(model: ReviewDetailViewModel(review: review, app: self.model.app))) ) {
+                            FeedReviewCard(review: review).listRowInsets(.init(top: 0, leading: 0, bottom: 0, trailing: 0)).onAppear() {
+                                if index == self.model.reviews.count - 25 {
+                                    self.model.getNextPage()
+                                }
                             }
-                        }
-                    }.listRowInsets(.init(top: 0, leading: 0, bottom: 0, trailing: 0))
-                }
-                HStack(){
-                    Spacer()
-                    ActivityIndicatorView(isVisible: $model.isLoading, type: .arcs).frame(width:50, height:50)
-                    Spacer()
-                }
-                
-            }.pullToRefresh(isShowing: $model.isRefreshing){
-                self.model.refreshUserFeed()
-            }.onAppear() {
-                if #available(iOS 14.0, *) {
-                    // iOS 14 doesn't have extra separators below the list by default.
-                } else {
-                    // To remove only extra separators below the list:
-                    UITableView.appearance().tableFooterView = UIView()
-                }
+                        }.listRowInsets(.init(top: 0, leading: 0, bottom: 0, trailing: 0))
+                    }
+                    HStack(){
+                        Spacer()
+                        ActivityIndicatorView(isVisible: $model.isLoading, type: .arcs).frame(width:50, height:50)
+                        Spacer()
+                    }
+                    
+                }.pullToRefresh(isShowing: $model.isRefreshing){
+                    self.model.refreshUserFeed()
+                }.onAppear() {
+                    if #available(iOS 14.0, *) {
+                        // iOS 14 doesn't have extra separators below the list by default.
+                    } else {
+                        // To remove only extra separators below the list:
+                        UITableView.appearance().tableFooterView = UIView()
+                    }
 
-                // To remove all separators including the actual ones:
-                UITableView.appearance().separatorStyle = .none
-                UITableViewCell.appearance().selectionStyle = .none
-            }.navigationBarTitle("Rave")
+                    // To remove all separators including the actual ones:
+                    UITableView.appearance().separatorStyle = .none
+                    UITableViewCell.appearance().selectionStyle = .none
+                }.navigationBarTitle("Rave")
 
+            }
+            HStack(alignment:.bottom) {
+                Spacer()
+                VStack(alignment:.trailing) {
+                    Spacer()
+                    NewRaveButton().padding()
+                }
+            }
         }
     }
 }
