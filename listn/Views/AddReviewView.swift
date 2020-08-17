804152de1721f4c4cd395cbcb632d93d4e64e273
Pierre Rodgers <pierre.rodgers.1@gmail.com>
2020-08-17 12:23:50 +1000

    Update AddReviewView.swift

--- listn/Views/AddReviewView.swift
+++ listn/Views/AddReviewView.swift
@@ -7,21 +7,38 @@
 //
 
 import SwiftUI
-import RealmSwift
-import Realm
+import SDWebImageSwiftUI
+
+struct NewReview {
+    var score : Int = 0
+    var text : String = ""
+}
 
 struct AddReviewView: View {
     @ObservedObject var model : AddReviewViewModel
-    
+    @State var review  = NewReview()
     
     
     var body: some View {
-        VStack {
-            Text("Add review")
-            Text("Album to test review for:")
-            Text(model.album.name) + Text (model.album.artist.name)
-            Button(action:self.model.postReview) {
-                Text("Press to add review")
+        ScrollView {
+            VStack(alignment:.leading) {
+                Text("Rave about").title()
+                HStack{
+                    Spacer()
+                    WebImage(url:URL(string: self.model.album.smallArtwork ?? "")).resizable().placeholder(){Rectangle()}.frame(width:300, height:300)
+                    Spacer()
+                }
+                Text(model.album.name).bold()
+                Text(model.album.artist.name)
+                Divider()
+                Text("Score").font(.headline)
+                Picker(selection: $review.score, label: Text("Score")) {
+                    ForEach(1...100, id:\.self) { score in
+                        Text(String(score))
+                    }
+                }
+                TextField("Add details", text: $review.text)
+                SaveReviewButton(action:{self.model.postReview(review: self.review)})
             }
         }
     }
