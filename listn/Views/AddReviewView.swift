d309ed0e68d55d01b17e0cec23aa65988f8d2d40
Pierre Rodgers <pierre.rodgers.1@gmail.com>
2020-08-24 14:09:01 +1000

    Update AddReviewView.swift
    Add alert popup

--- listn/Views/AddReviewView.swift
+++ listn/Views/AddReviewView.swift
@@ -9,6 +9,7 @@
 import SwiftUI
 import SDWebImageSwiftUI
 import TextView
+import PopupView
 
 struct NewReview {
     var score : Int = 0
@@ -23,44 +24,54 @@ struct AddReviewView: View {
     @State var review  = NewReview()
     
     @State var isEditing : Bool = false
-    
+    @State var showPopup = false
     
     var body: some View {
         NavigationView {
-            ScrollView {
-                VStack(alignment:.leading) {
-                    if model.inputValid.missingAlbum {
-                        Button(action:{}) {
-                            Text("Add album")
+            ZStack {
+                ScrollView {
+                    VStack(alignment:.leading) {
+                        if model.inputValid.missingAlbum {
+                            Button(action:{
+                                self.showPopup = false
+                                self.showPopup = true
+                            }) {
+                                Text("Add album")
+                            }
                         }
-                    }
-                    else {
-                        HStack{
-                            Spacer()
-                            WebImage(url:URL(string: self.model.album!.smallArtwork ?? "")).resizable().placeholder(){Rectangle()}.frame(width:300, height:300)
-                            Spacer()
+                        else {
+                            HStack{
+                                Spacer()
+                                WebImage(url:URL(string: self.model.album!.smallArtwork ?? "")).resizable().placeholder(){Rectangle()}.frame(width:300, height:300)
+                                Spacer()
+                            }
+                            Text(model.album!.name).bold()
+                            Text(model.album!.artist.name)
                         }
-                        Text(model.album!.name).bold()
-                        Text(model.album!.artist.name)
-                    }
-                    Divider()
-                    Text("Score").font(.headline)
-                    Picker(selection: $review.score, label: Text("Score")) {
-                        ForEach(1...100, id:\.self) { score in
-                            Text(String(score))
+                        Divider()
+                        Text("Score").font(.headline)
+                        Picker(selection: $review.score, label: Text("Score")) {
+                            ForEach(1...100, id:\.self) { score in
+                                Text(String(score))
+                            }
                         }
+                        TextView(text: $review.text, isEditing: $isEditing, placeholder: "Add details").frame(minHeight:500, maxHeight:.infinity)
+                        
                     }
-                    TextView(text: $review.text, isEditing: $isEditing, placeholder: "Add details").frame(minHeight:500, maxHeight:.infinity)
-                    
-                }
-            }.navigationBarTitle("Rave about")
-                .navigationBarItems(leading:Button(action:{self.showReviewSheet.isAddingReview = false}){
-                    Text("Cancel").foregroundColor(.red)
-                },
-                trailing: SaveReviewButton(action:{
-                    self.model.postReview(review: self.review)
-                    self.showReviewSheet.isAddingReview = false
+                }.navigationBarTitle("Rave about")
+                    .navigationBarItems(leading:Button(action:{self.showReviewSheet.isAddingReview = false}){
+                        Text("Cancel").foregroundColor(.red)
+                    },
+                    trailing: SaveReviewButton(action:{
+                        self.model.postReview(review: self.review)
+                        self.showReviewSheet.isAddingReview = false
                 }))
+            }.popup(isPresented: $showPopup, view: {
+                VStack{
+                    Text("This feature not implemented yet!").bold()
+                    Text("To review an album, search for it first!")
+                }.background(Color.red).frame(width:200, height:100)
+            })
         }
     }
     
